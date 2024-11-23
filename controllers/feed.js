96f0929b9d04fd57f22981d41e138c4f68b543dd
mahmoudomar1337 <rickgrimestwd1337@gmail.com>
2024-11-23 19:16:21 +0200

    fixing missing username

--- controllers/feed.js
+++ controllers/feed.js
@@ -10,6 +10,7 @@ exports.getPosts = async (req, res, next) => {
   try {
     const totalItems = await Post.find().countDocuments();
     const posts = await Post.find()
+      .populate("creator")
       .skip((currentPage - 1) * perPage)
       .limit(perPage);
 
@@ -71,7 +72,7 @@ exports.createPost = async (req, res, next) => {
 exports.getPost = async (req, res, next) => {
   const postId = req.params.postId;
   try {
-    const post = await Post.findById(postId);
+    const post = await Post.findById(postId).populate("creator");
     if (!post) {
       const error = new Error("Coud not find post");
       error.statusCode = 404;
